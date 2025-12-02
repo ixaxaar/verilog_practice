29a4180bf7d90a938f84e1fbd7db4f2e7389654f
ixaxaar <root@ixaxaar.in>
2025-12-02 19:32:44 +0530

    uniform output name

--- demux.v
+++ demux.v
@@ -5,13 +5,13 @@ module demux_reg #(
     input wire clk,
     input wire [SEL_WIDTH-1:0] sel,
     input wire [DATA_WIDTH-1:0] in,                         // Single input
-    output reg [DATA_WIDTH*(2**SEL_WIDTH)-1:0] outputs      // Multiple outputs (packed)
+    output reg [DATA_WIDTH*(2**SEL_WIDTH)-1:0] out      // Multiple out (packed)
 );
 
     // Route input to selected output, others get 0
     always @(posedge clk) begin
-        outputs <= 0;
-        outputs[DATA_WIDTH*sel +: DATA_WIDTH] <= in;
+        out <= 0;
+        out[DATA_WIDTH*sel +: DATA_WIDTH] <= in;
     end
 endmodule
 
@@ -21,7 +21,7 @@ module demux_wire #(
 )(
     input wire [SEL_WIDTH-1:0] sel,
     input wire [DATA_WIDTH-1:0] in,                         // Single input
-    output wire [DATA_WIDTH*(2**SEL_WIDTH)-1:0] outputs      // Multiple outputs (packed)
+    output wire [DATA_WIDTH*(2**SEL_WIDTH)-1:0] out      // Multiple out (packed)
 );
 
     reg [DATA_WIDTH*(2**SEL_WIDTH)-1:0] result_reg;
@@ -32,5 +32,5 @@ module demux_wire #(
         result_reg[DATA_WIDTH*sel +: DATA_WIDTH] = in;
     end
 
-    assign outputs = result_reg;
+    assign out = result_reg;
 endmodule
